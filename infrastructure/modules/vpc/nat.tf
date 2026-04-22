4b67a84ba794a68d1eed17aa61f973ef0a49bee7
Muhammad Ali <muhammad.ali@hearseek.com>
2026-04-22 17:16:41 +0500

    VPC: Cloud Router + NAT + firewall rules

---
+++ infrastructure/modules/vpc/nat.tf
@@ -0,0 +1,16 @@
+resource "google_compute_router_nat" "nat" {
+  name                               = "${var.vpc_name}-nat"
+  router                             = var.router_name
+  region                             = var.region
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
+  subnetwork {
+    name                    = google_compute_subnetwork.private.name
+    source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
+  }
+
+  log_config {
+    enable = true
+    filter = "ERRORS_ONLY"
+  }
+}
